a863167da0c75726274915a19d051da618c830d0
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-12-31 11:16:01 +0100

    project init

---
+++ sonar/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+
+  { path: 'catalog', loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
