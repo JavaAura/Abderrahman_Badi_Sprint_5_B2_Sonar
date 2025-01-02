9af012fb8ab24b54da56ddb4e37b0d1c66197f95
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2025-01-02 11:28:25 +0100

    Music model and category enum

---
+++ sonar/src/app/core/enums/music-category.enum.ts
@@ -0,0 +1,10 @@
+export enum MusicCategory {
+    POP = 'pop',
+    ROCK = 'rock',
+    RAP = 'rap',
+    CHAABI = 'cha3bi',
+    JAZZ = 'jazz',
+    CLASSICAL = 'classical',
+    RNB = 'rnb',
+    ELECTRONIC = 'electronic'
+}
