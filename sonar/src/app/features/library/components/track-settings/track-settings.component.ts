e33150c996e174b76bd538155db6bba075dce12e
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2025-01-12 20:38:59 +0100

    SN-24 track: added settings seb menu

--- sonar/src/app/features/library/components/track-settings/track-settings.component.ts
+++ sonar/src/app/features/library/components/track-settings/track-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Track } from '../../../track/state/track.model';
 
 @Component({
   selector: 'app-track-settings',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './track-settings.component.scss'
 })
 export class TrackSettingsComponent {
+  @Input() track!: Track;
+  @Output() open = new EventEmitter<Track>();
+  isOpen: boolean = false
 
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.menu-button')) {
+      this.isOpen = false;
+    }
+  }
+
+
+  editTrack() {
+    this.open.emit(this.track);
+  }
 }
